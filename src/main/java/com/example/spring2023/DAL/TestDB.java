5c780eab2c80e872b190f6d39ee99387fb0bbcc5
Iceneo <nosminin@rit-it.com>
2023-10-02 20:48:49 +0500

    Architecture class added + mock data

---
+++ src/main/java/com/example/spring2023/DAL/TestDB.java
@@ -0,0 +1,7 @@
+package com.example.spring2023.DAL;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TestDB {
+}
---
+++ src/main/java/com/example/spring2023/Services/TestService.java
@@ -0,0 +1,7 @@
+package com.example.spring2023.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+}
